124c7d71abc2572b5e86040b67b83b3a5bdb710e
Cristian Merli <c.merli1997@gmail.com>
2021-03-11 13:28:54 +0100

    Lecture 02 modifications

--- 02_Lecture/printfdimemsioni.c
+++ 02_Lecture/printfdimemsioni.c
@@ -21,14 +21,14 @@ int main(){ // Main cycle
   long double h; // LONG DOUBLE (C standard data type) variable
 
   printf("\n> Memo requirement for standard C data types:\n\n");
-  printf("- CHAR          -->     sizeof(a) in bytes = %lu, %lu bits\n", sizeof(a), sizeof(a)*8); // Print size of a (CHAR) in bytes and bits
-  printf("- INT           -->     sizeof(b) in bytes = %lu, %lu bits\n", sizeof(b), sizeof(b)*8); // Print size of b (INT) in bytes and bits
-  printf("- SHORT         -->     sizeof(c) in bytes = %lu, %lu bits\n", sizeof(c), sizeof(c)*8); // Print size of c (SHORT) in bytes and bits
-  printf("- LONG          -->     sizeof(d) in bytes = %lu, %lu bits\n", sizeof(d), sizeof(d)*8); // Print size of d (LONG) in bytes and bits
-  printf("- LONG LONG     -->     sizeof(e) in bytes = %lu, %lu bits\n", sizeof(e), sizeof(e)*8); // Print size of e (LONG LONG) in bytes and bits
-  printf("- FLOAT         -->     sizeof(f) in bytes = %lu, %lu bits\n", sizeof(f), sizeof(f)*8); // Print size of f (FLOAT) in bytes and bits
-  printf("- DOUBLE        -->     sizeof(g) in bytes = %lu, %lu bits\n", sizeof(g), sizeof(g)*8); // Print size of g (DOUBLE) in bytes and bits
-  printf("- LONG DOUBLE   -->     sizeof(h) in bytes = %lu, %lu bits\n", sizeof(h), sizeof(h)*8); // Print size of h (LONG DOUBLE) in bytes and bits
+  printf("- CHAR          -->     sizeof(char) in bytes = %lu           (%lu bits)\n", sizeof(a), sizeof(a)*8); // Print size of a (CHAR) in bytes and bits
+  printf("- INT           -->     sizeof(int) in bytes = %lu            (%lu bits)\n", sizeof(b), sizeof(b)*8); // Print size of b (INT) in bytes and bits
+  printf("- SHORT         -->     sizeof(short) in bytes = %lu          (%lu bits)\n", sizeof(c), sizeof(c)*8); // Print size of c (SHORT) in bytes and bits
+  printf("- LONG          -->     sizeof(long) in bytes = %lu           (%lu bits)\n", sizeof(d), sizeof(d)*8); // Print size of d (LONG) in bytes and bits
+  printf("- LONG LONG     -->     sizeof(long long) in bytes = %lu      (%lu bits)\n", sizeof(e), sizeof(e)*8); // Print size of e (LONG LONG) in bytes and bits
+  printf("- FLOAT         -->     sizeof(float) in bytes = %lu          (%lu bits)\n", sizeof(f), sizeof(f)*8); // Print size of f (FLOAT) in bytes and bits
+  printf("- DOUBLE        -->     sizeof(double) in bytes = %lu         (%lu bits)\n", sizeof(g), sizeof(g)*8); // Print size of g (DOUBLE) in bytes and bits
+  printf("- LONG DOUBLE   -->     sizeof(long double) in bytes = %lu   (%lu bits)\n", sizeof(h), sizeof(h)*8);  // Print size of h (LONG DOUBLE) in bytes and bits
 
   return 0; // Check errors --> if=0 (NO ERRORS) / if=1 (ERRORS)
 } // End main cycle
@@ -38,7 +38,7 @@ int main(){ // Main cycle
 // Links //
 /*
   Classroom:
-  https://classroom.google.com/u/0/w/NjE5MzUzMTIwMzJa/t/all
+  https://classroom.google.com/u/0/c/Mjg1NDUwNDU3NTE2
 
   Moodle:
   https://didatticaonline.unitn.it/dol/course/view.php?id=31321

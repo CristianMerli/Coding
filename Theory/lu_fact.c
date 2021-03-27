52471c137b9609d206415def86a9a46678b62132
Cristian Merli <c.merli1997@gmail.com>
2021-03-27 10:05:58 +0100

    LU factorization algorithm modifications

--- Theory/lu_fact.c
+++ Theory/lu_fact.c
@@ -31,6 +31,7 @@ const char E[] = "\033[0m";                                                     
 /* Functions declaration and definition */
 void logo(unsigned char vthck, unsigned char lthck_vthik_r, unsigned char start_sp, unsigned char lsp,
           char txt[], const char txt_col[], char bkg_chr, const char bkg_col[]){                            // Print logo function
+  /* Function body */
   printf("\n%s", bkg_col);                                                                                  // New line
   unsigned char len = 2*lthck_vthik_r*vthck+2*lsp+strlen(txt);                                              // Logo length calc
   for (unsigned char i = 0; i < (4*vthck+1); i++){                                                          // Logo lines print FOR cycle
@@ -87,6 +88,7 @@ void mat_vect_init(double a[n][n], unsigned char p[n], double b[n], double x[n],
 }
 
 void print_mat_vect(double a[n][n], unsigned char p[n], double b[n], double x[n], unsigned char n){         // Matrix and vectors print function
+  /* Function body */
   printf("\n\n%s--> %s%dx%d%s matrix and %s%dx1%s vectors: %s\n", O, B, n, n, C, B, n, C, E);               // Print mat and vect info
   for (unsigned char i = 0; i < n; i++){                                                                    // Mat and vect lines print FOR cycle
     for (unsigned char j = 0; j < n; j++){                                                                  // Mat columns print FOR cycle
@@ -135,6 +137,7 @@ void lu_fact(double a[n][n], unsigned char p[n], unsigned char n){              
 }
 
 void solve(double a[n][n], unsigned char p[n], double b[n], double x[n], unsigned char n){                  // Solve function
+  /* Function body */
   for (unsigned char i = 0; i < n; i++){                                                                    // Apply permutations on known terms vector (p*b) FOR cycle and solve IT system (p*a*x=p*b) = (L*z=p*b) --> Solve Inferior-Triangular system lines FOR cycle
     x[i] = b[p[i]-1];                                                                                       // Apply permutations on known terms vector (p*b) --> Store p*b in x vector to save memo
     if (i >= 1){                                                                                            // After first line (z1 = b1), isolate the other zi stored in xi
@@ -156,8 +159,8 @@ void solve(double a[n][n], unsigned char p[n], double b[n], double x[n], unsigne
 /* Main cycle */
 int main(){
   /* Vars declaration and definition */
-  //double a[5][5] = {{0, 1, 0, 1, 2}, {6, 0, -1, 2, 3}, {1, 1, 0, 0, 1}, {0, -1, 1, 1, 0}, {-1, 0, 0, 2, 3}, };// Test matrix declaration and definition (b4 set n=5 and comment from line 79 to 84 included)
-  //double b[5] = {4, 6, 4, 1, 2};                                                                              // Known terms test vector declaration and definition (b4 set n=5 and comment from line 79 to 84 included)
+  //double a[5][5] = {{0, 1, 0, 1, 2}, {6, 0, -1, 2, 3}, {1, 1, 0, 0, 1}, {0, -1, 1, 1, 0}, {-1, 0, 0, 2, 3}, };// Test matrix declaration and definition (b4 set n=5 and comment from line 80 to 85 included)
+  //double b[5] = {4, 6, 4, 1, 2};                                                                              // Known terms test vector declaration and definition (b4 set n=5 and comment from line 80 to 85 included)
   double a[n][n];                                                                                           // Matrix declaration
   double b[n];                                                                                              // Known terms vector declaration
   double x[n];                                                                                              // Unknown terms vector declaration

dfab426084206249976e649ed2209c1e51aaa972
Cristian Merli <c.merli1997@gmail.com>
2022-04-30 13:45:31 +0200

    HW1 structure created

---
+++ C++/HW1/lib/cnn/lib_cnn.h
@@ -0,0 +1,36 @@
+/*
+ * Author: Cristian Merli
+ * Code title: ConvNet library header file
+ * Code version: 0.0
+ * Creation date: 30/04/2022
+ * Last mod. date: 30/04/2022 
+ */
+
+
+/* Include guards */
+#pragma once                                                                                                // Avoid multiple inclusions
+#ifndef _CONVNET_H_                                                                                         // Avoid multiple inclusions (old-alternative start)
+#define _CONVNET_H_                                                                                         // Avoid multiple inclusions (old-alternative)
+
+
+/* Dependencies */
+#include "../ui/lib_ui.h"                                                                                   // Import UI (terminal I/O) library header file
+
+
+/* Libraries */
+//
+
+
+/* Constants */
+//
+
+
+/* Macros */
+//
+
+
+/* Public functions */
+//
+
+
+#endif                                                                                                      // Avoid multiple inclusions (old-alternative end)
